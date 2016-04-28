687c74555fada685022f47d244fa7004e526f7eb
bauzana <bauzana@uninorte.edu.co>
2016-04-28 10:36:47 -0500

    Added files via upload

---
+++ app/Models/UsuarioSchema.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UsuarioSchema = new Schema({
+    Name: String,
+    LastName: String,
+    Email:String,
+    Telefono:Number,
+    User:String,
+    Contrasena:String,
+    Codigo:Number,,
+    Carrera:String,
+    Semestre:Number
+    Amigos: [
+    	{ type: Schema.Types.ObjectId, ref: "Users" }
+    ]
+});
+module.exports = mongoose.model('Users', UsuarioSchema);
