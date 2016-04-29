dc30a17d4c62f79d03dde56bef54c28d3b13998c
Carlos Díaz Berrio <carlosberrio08@hotmail.com>
2016-04-28 19:16:53 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('LinkeInUninorte', ['appRoutes', 'ngCookies', 'MainCtrl', 'LoginCtrl', 'OrderCtrl', 'PayCtrl', 'RegisterCtrl']);
+var app = angular.module('LinkedInUninorte', ['appRoutes', 'ngCookies', 'MainCtrl', 'LoginCtrl', 'RegisterCtrl']);
 app.run(["$rootScope", "$cookieStore", "$http", "$state", function($rootScope, $cookieStore, $http, $state) {
     $rootScope.globals = $cookieStore.get("globals") || {};
     if ($rootScope.globals.currentUser) {
